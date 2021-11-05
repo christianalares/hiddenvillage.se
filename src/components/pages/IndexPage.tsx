b3e3d6c921e2ab106dd37ca738ddddcb32f2ce60
Christian Alares <christian.alares@gmail.com>
2021-11-05 16:45:24 +0100

    fix center position

--- src/components/pages/IndexPage.tsx
+++ src/components/pages/IndexPage.tsx
@@ -1,8 +1,6 @@
 const IndexPage = () => {
   return (
-    <div className="grid place-content-center w-screen h-screen">
-      <h1>Hidden Village</h1>
-    </div>
+    <h1 className="absolute top-1/2 w-full text-center transform -translate-y-1/2 animate-bounce">Hidden Village</h1>
   )
 }
 
