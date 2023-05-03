eb3a25ec0c440ea0890450a13d010f7bdc6d69e2
Christian Alares <christian.alares@gmail.com>
2023-05-03 13:08:20 +0200

    add awaited exit animation with useAnimate

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,33 +3,68 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { useRouter } from 'next/navigation'
 import { Icon } from '@/components/Icon'
-import { motion } from 'framer-motion'
+import { motion, useAnimate } from 'framer-motion'
 
 type Props = {
   children: React.ReactNode
   isOpen: boolean
 }
 
+const overlayVariants = {
+  close: {
+    opacity: 0,
+  },
+  open: {
+    opacity: 1,
+  },
+}
+
+const dialogVariants = {
+  close: {
+    opacity: 0,
+    scale: 0.95,
+    y: '5%',
+  },
+  open: {
+    opacity: 1,
+    scale: 1,
+    y: 0,
+  },
+}
+
 export const Modal = ({ children, isOpen }: Props) => {
   const router = useRouter()
+  const [overlayScope, animateOverlay] = useAnimate()
+  const [dialogScope, animateDialog] = useAnimate()
+
+  const handleClose = async () => {
+    await Promise.all([
+      animateOverlay(overlayScope.current, overlayVariants.close),
+      animateDialog(dialogScope.current, dialogVariants.close),
+    ])
+
+    router.back()
+  }
 
   return (
-    <Dialog.Root modal open={isOpen} onOpenChange={() => router.back()}>
+    <Dialog.Root modal open={isOpen} onOpenChange={handleClose}>
       <Dialog.Portal forceMount>
         <Dialog.Overlay asChild>
           <motion.div
             className="fixed inset-0 bg-black/30 backdrop-blur-sm"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.2 }}
+            ref={overlayScope}
+            initial={overlayVariants.close}
+            animate={overlayVariants.open}
+            transition={{ duration: 0.1 }}
           />
         </Dialog.Overlay>
         <Dialog.DialogContent asChild>
           <motion.div
-            initial={{ opacity: 0, scale: 0.95, y: '10%' }}
-            animate={{ opacity: 1, scale: 1, y: 0 }}
-            transition={{ duration: 0.2 }}
             className="fixed inset-3 z-10 mx-auto max-w-2xl overflow-y-scroll rounded-md bg-white/10 backdrop-blur-lg md:top-[8vh] md:h-fit md:max-h-[85vh]"
+            ref={dialogScope}
+            initial={dialogVariants.close}
+            animate={dialogVariants.open}
+            transition={{ duration: 0.1 }}
           >
             <Dialog.Close asChild>
               <button
